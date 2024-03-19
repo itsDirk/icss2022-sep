89a34ee387373cd9c865d7a115ac12016360320a
Dirk <93925356+itsDirk@users.noreply.github.com>
2024-03-19 08:12:11 +0100

    Update ASTListener.java
    Start progress on AST walker

--- startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -3,6 +3,7 @@ package nl.han.ica.icss.parser;
 import java.util.Stack;
 
 
+import nl.han.ica.datastructures.HANStack;
 import nl.han.ica.datastructures.IHANStack;
 import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.literals.*;
@@ -12,6 +13,7 @@ import nl.han.ica.icss.ast.operations.SubtractOperation;
 import nl.han.ica.icss.ast.selectors.ClassSelector;
 import nl.han.ica.icss.ast.selectors.IdSelector;
 import nl.han.ica.icss.ast.selectors.TagSelector;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 /**
  * This class extracts the ICSS Abstract Syntax Tree from the Antlr Parse tree.
@@ -26,10 +28,62 @@ public class ASTListener extends ICSSBaseListener {
 
 	public ASTListener() {
 		ast = new AST();
-		//currentContainer = new HANStack<>();
+		currentContainer = new HANStack<>();
+		currentContainer.push(ast.root);
 	}
     public AST getAST() {
         return ast;
     }
-    
+
+//	public void enterStylesheet(ICSSParser.StylesheetContext ctx) {
+//		Stylesheet stylesheet = new Stylesheet();
+////		ast.root.addChild(stylesheet);
+//		currentContainer.push(stylesheet);
+//	}
+//
+//	public void exitStylesheet(ICSSParser.StylesheetContext ctx) {
+//		Stylesheet stylesheet = (Stylesheet) currentContainer.pop();
+////		currentContainer.peek().addChild(stylesheet);
+//		currentContainer.pop();
+//	}
+
+	// RuleAssignement
+	@Override
+	 public void enterRuleAssignement(ICSSParser.RuleAssignementContext ctx) {
+		Stylerule stylerule = new Stylerule();
+		// ERGENS een ctx.getText() ofzo
+		currentContainer.push(stylerule);
+	 }
+	 @Override
+	 public void exitRuleAssignement(ICSSParser.RuleAssignementContext ctx) {
+		Stylerule stylerule = (Stylerule) currentContainer.pop();
+		currentContainer.peek().addChild(stylerule);
+	 }
+
+	// Selector
+	@Override
+	public void enterSelector(ICSSParser.SelectorContext ctx) {
+		//if ctx . is class dinv
+		//then n
+
+//		ParseTree selector = ctx.getChild(0);
+
+		if (ctx.getChild(0) instanceof TagSelector) {
+			TagSelector tagSelector = new TagSelector(ctx.getChild(0).getText());
+			currentContainer.push(tagSelector);
+		}
+		else if (ctx.getChild(0) instanceof IdSelector) {
+			IdSelector idSelector = new IdSelector(ctx.getChild(0).getText());
+			currentContainer.push(idSelector);
+		}
+		else if (ctx.getChild(0) instanceof ClassSelector) {
+			ClassSelector classSelector = new ClassSelector(ctx.getChild(0).getText());
+			currentContainer.push(classSelector);
+		}
+	}
+	@Override
+	public void exitSelector(ICSSParser.SelectorContext ctx) {
+		Selector selector = (Selector) currentContainer.pop();
+		currentContainer.peek().addChild(selector);
+	}
 }
