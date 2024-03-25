8939a62cee51aae562ea4b39c33fe3b90989ed8d
Dirk <93925356+itsDirk@users.noreply.github.com>
2024-03-25 11:52:33 +0100

    Typte evaluator mee in de les

--- startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
+++ startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
@@ -23,8 +23,37 @@ public class Evaluator implements Transform {
     @Override
     public void apply(AST ast) {
         //variableValues = new HANLinkedList<>();
+        applyStyleSheet(ast.root);
 
     }
 
-    
+    private void applyStyleSheet(Stylesheet stylesheet) {
+        applyStylerule((Stylerule) stylesheet.getChildren().get(0));
+    }
+
+    private void applyStylerule(Stylerule rule) {
+        for (ASTNode node : rule.getChildren()) {
+            if (node instanceof Declaration) {
+                applyDeclaration((Declaration) node);
+            }
+        }
+    }
+
+    private void applyDeclaration(Declaration declaration) {
+        declaration.expression = evaluateExpression(declaration.expression);
+    }
+
+    private Expression evaluateExpression(Expression expression) {
+       if (expression instanceof Literal) {
+           return (Literal) expression;
+       } else {
+           return evaluateAddOperation((AddOperation) expression);
+       }
+    }
+
+    private Expression evaluateAddOperation(AddOperation expression) {
+        PixelLiteral left = (PixelLiteral) evaluateExpression(expression.lhs);
+        PixelLiteral right = (PixelLiteral) evaluateExpression(expression.rhs);
+        return new PixelLiteral(left.value + right.value);
+    }
 }
