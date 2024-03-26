33b50985aa50e59fdb075252b9ecebe506d54262
Dirk <93925356+itsDirk@users.noreply.github.com>
2024-03-26 11:19:41 +0100

    Update Checker.java
    Voegde checkVariableAssignment en checkLiteral toe

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -3,14 +3,11 @@ package nl.han.ica.icss.checker;
 import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
-import nl.han.ica.icss.ast.literals.PercentageLiteral;
-import nl.han.ica.icss.ast.literals.PixelLiteral;
-import nl.han.ica.icss.ast.literals.ScalarLiteral;
+import nl.han.ica.icss.ast.literals.*;
 import nl.han.ica.icss.ast.operations.AddOperation;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
-
 
 
 public class Checker {
@@ -23,44 +20,49 @@ public class Checker {
     }
 
     private void checkStylesheet(Stylesheet Stylesheet) {
+        variableTypes.addFirst(new HashMap<>());
         for (ASTNode node : Stylesheet.getChildren()) {
-            if (node instanceof Stylerule) {
+            if (node instanceof VariableAssignment) {
+                checkVariableAssignment((VariableAssignment) node);
+            } else if (node instanceof Stylerule) {
                 checkStylerule((Stylerule) node);
+            } else {
+                node.setError("Ongeldig type: verwachtte VariableAssignment of Stylerule");
             }
+        }
+        variableTypes.removeFirst();
+    }
+
+    private void checkVariableAssignment(VariableAssignment variableAssignment) {
+        if (variableAssignment.expression instanceof BoolLiteral) {
+            variableTypes.getFirst().put(variableAssignment.name.name, ExpressionType.BOOL);
+        } else if (variableAssignment.expression instanceof ColorLiteral) {
+            variableTypes.getFirst().put(variableAssignment.name.name, ExpressionType.COLOR);
+        } else if (variableAssignment.expression instanceof PixelLiteral) {
+            variableTypes.getFirst().put(variableAssignment.name.name, ExpressionType.PIXEL);
+        } else if (variableAssignment.expression instanceof PercentageLiteral) {
+            variableTypes.getFirst().put(variableAssignment.name.name, ExpressionType.PERCENTAGE);
+        } else if (variableAssignment.expression instanceof ScalarLiteral) {
+            variableTypes.getFirst().put(variableAssignment.name.name, ExpressionType.SCALAR);
+        } else {
+            variableAssignment.setError("Ongeldig type: verwachtte Bool, Color, Pixel, Percentage of Scalar");
         }
     }
 
     private void checkStylerule(Stylerule stylerule) {
+        variableTypes.addFirst(new HashMap<>());
         for (ASTNode node : stylerule.getChildren()) {
-            if (node instanceof Declaration) {
+            if (node instanceof VariableAssignment) {
+                checkVariableAssignment((VariableAssignment) node);
+            } else if (node instanceof Declaration) {
                 checkDeclaration((Declaration) node);
+            } else {
+                node.setError("Ongeldig type: verwachtte VariableAssignment of Declaration");
             }
         }
-    }
-
-    private void checkDeclaration(Declaration declaration) {
-        if (declaration.expression instanceof Literal) {
-            declaration.expression = (Literal) declaration.expression;
-        } else {
-            declaration.expression = checkAddOperation((AddOperation) declaration.expression);
-        }
-    }
-
-private Expression checkAddOperation(AddOperation expression) {
-        if (expression.lhs instanceof PixelLiteral && expression.rhs instanceof PixelLiteral) {
-            return new PixelLiteral(((PixelLiteral) expression.lhs).value + ((PixelLiteral) expression.rhs).value);
-        } else if (expression.lhs instanceof PercentageLiteral && expression.rhs instanceof PercentageLiteral) {
-            return new PercentageLiteral(((PercentageLiteral) expression.lhs).value + ((PercentageLiteral) expression.rhs).value);
-        } else if (expression.lhs instanceof ScalarLiteral && expression.rhs instanceof ScalarLiteral) {
-            return new ScalarLiteral(((ScalarLiteral) expression.lhs).value + ((ScalarLiteral) expression.rhs).value);
-        } else {
-            return null;
-        }
+        variableTypes.removeFirst();
     }
 
 
 
-
-
-    
 }
