771dba79e01159a4547e50ddb5ba37ff6843c3c1
Dirk <93925356+itsDirk@users.noreply.github.com>
2024-03-29 15:17:10 +0100

    Update Checker.java
    Refactoring code + operation checker

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -4,6 +4,9 @@ import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.literals.*;
+import nl.han.ica.icss.ast.operations.AddOperation;
+import nl.han.ica.icss.ast.operations.MultiplyOperation;
+import nl.han.ica.icss.ast.operations.SubtractOperation;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
@@ -33,6 +36,8 @@ public class Checker {
     }
 
     private void checkVariableAssignment(VariableAssignment variableAssignment) {
+        checkVariableName(variableAssignment.name);
+
         if (variableAssignment.expression instanceof BoolLiteral) {
             variableTypes.getFirst().put(variableAssignment.name.name, ExpressionType.BOOL);
         } else if (variableAssignment.expression instanceof ColorLiteral) {
@@ -45,6 +50,12 @@ public class Checker {
             variableTypes.getFirst().put(variableAssignment.name.name, ExpressionType.SCALAR);
         } else {
             variableAssignment.setError("Ongeldig type: verwachtte Bool, Color, Pixel, Percentage of Scalar");
+        }
+    }
+
+    private void checkVariableName(VariableReference name) {
+        if (!variableTypes.getFirst().containsKey(name.name)) {
+            name.setError("Variabele niet gedefinieerd: " + name.name);
         }
     }
 
@@ -63,7 +74,7 @@ public class Checker {
     }
 
     private void checkDeclaration(Declaration declaration) {
-        PropertyName propertyName = checkPropertyName(declaration.property);
+//        PropertyName propertyName = checkPropertyName(declaration.property);
         ExpressionType expressionType = checkExpression(declaration.expression);
 
         if (expressionType != ExpressionType.UNDEFINED) {
@@ -88,7 +99,6 @@ public class Checker {
                         declaration.setError("Background-color moet een kleur zijn");
                     }
                     break;
-
                 default:
                     declaration.setError("Ongeldige eigenschap: " + declaration.property.name);
             }
@@ -98,21 +108,26 @@ public class Checker {
     private ExpressionType checkExpression(Expression expression) {
         if (expression instanceof Literal) {
             return checkLiteral((Literal) expression);
+        } else if (expression instanceof VariableReference) {
+            return checkVariableReference((VariableReference) expression);
+        } else if (expression instanceof Operation) {
+            return checkOperation((Operation) expression);
         }
-        expression.setError("Ongeldig type: TODO");
+        expression.setError("Ongeldige expressie: " + expression.getClass().getSimpleName());
         return ExpressionType.UNDEFINED;
     }
 
-    private PropertyName checkPropertyName(PropertyName property) {
-        String[] validProperties = {"width", "height", "color", "background-color"};
-        for (String validProperty : validProperties) {
-            if (property.name.equals(validProperty)) {
-                return property;
-            }
-        }
-        property.setError("Ongeldige eigenschap: " + property.name);
-        return null;
-    }
+
+    //    private PropertyName checkPropertyName(PropertyName property) {
+//        String[] validProperties = {"width", "height", "color", "background-color"};
+//        for (String validProperty : validProperties) {
+//            if (property.name.equals(validProperty)) {
+//                return property;
+//            }
+//        }
+//        property.setError("Ongeldige eigenschap: " + property.name);
+//        return null;
+//    }
 
     private ExpressionType checkLiteral(Literal literal) {
         if (literal instanceof BoolLiteral) {
@@ -129,6 +144,72 @@ public class Checker {
         literal.setError("Ongeldig type: verwachtte Bool, Color, Pixel, Percentage of Scalar");
         return ExpressionType.UNDEFINED;
     }
+    private ExpressionType checkVariableReference(VariableReference variableReference) {
+        for (HashMap<String, ExpressionType> currentScope : variableTypes) {
+            if (currentScope.containsKey(variableReference.name)) {
+                return currentScope.get(variableReference.name);
+            }
+        }
+        variableReference.setError("Variabele niet gedefinieerd: " + variableReference.name);
+        return ExpressionType.UNDEFINED;
+    }
+
+    private ExpressionType checkOperation(Operation operation) {
+        for (ASTNode child : operation.getChildren()) {
+            if (child instanceof BoolLiteral || child instanceof ColorLiteral) {
+                child.setError("Ongeldig operatietype: " + child.getClass().getSimpleName());
+                return ExpressionType.UNDEFINED;
+            }
+        }
+
+        if (operation instanceof MultiplyOperation) {
+            return checkMultiplyOperation((MultiplyOperation) operation);
+        } else if (operation instanceof AddOperation) {
+            return checkAddOperation((AddOperation) operation);
+        } else if (operation instanceof SubtractOperation) {
+            return checkSubtractOperation((SubtractOperation) operation);
+        }
+        operation.setError("Ongeldig operatietype: " + operation.getClass().getSimpleName());
+        return ExpressionType.UNDEFINED;
+    }
+
+    private ExpressionType checkMultiplyOperation(MultiplyOperation operation) {
+        ExpressionType leftType = checkExpression(operation.lhs);
+        ExpressionType rightType = checkExpression(operation.rhs);
+
+        if (leftType == rightType) {
+            return leftType;
+        } else {
+            operation.setError("Ongeldige operatie: " + leftType + " * " + rightType);
+            return ExpressionType.UNDEFINED;
+        }
+    }
+
+    private ExpressionType checkAddOperation(AddOperation operation) {
+        ExpressionType leftType = checkExpression(operation.lhs);
+        ExpressionType rightType = checkExpression(operation.rhs);
+
+        if (leftType == rightType) {
+            return leftType;
+        } else {
+            operation.setError("Ongeldige operatie: " + leftType + " + " + rightType);
+            return ExpressionType.UNDEFINED;
+        }
+    }
+
+    private ExpressionType checkSubtractOperation(SubtractOperation operation) {
+        ExpressionType leftType = checkExpression(operation.lhs);
+        ExpressionType rightType = checkExpression(operation.rhs);
+
+        if (leftType == rightType) {
+            return leftType;
+        } else {
+            operation.setError("Ongeldige operatie: " + leftType + " - " + rightType);
+            return ExpressionType.UNDEFINED;
+        }
+    }
+
+
 
 
 }
