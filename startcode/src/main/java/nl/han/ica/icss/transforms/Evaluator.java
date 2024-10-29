af1268f39843580bc7126f31758b1b1676e4c778
Dirk <d.vanroosmalen@student.han.nl>
2024-10-29 22:31:05 +0100

    Evaluator down variable assignments

--- startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
+++ startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
@@ -1,5 +1,6 @@
 package nl.han.ica.icss.transforms;
 
+import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.literals.PercentageLiteral;
@@ -10,50 +11,154 @@ import nl.han.ica.icss.ast.operations.MultiplyOperation;
 import nl.han.ica.icss.ast.operations.SubtractOperation;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 
 public class Evaluator implements Transform {
 
     private IHANLinkedList<HashMap<String, Literal>> variableValues;
 
     public Evaluator() {
-        //variableValues = new HANLinkedList<>();
+        variableValues = new HANLinkedList<>();
     }
 
     @Override
     public void apply(AST ast) {
-        //variableValues = new HANLinkedList<>();
-        applyStyleSheet(ast.root);
-
+        evaluateStylesheet(ast.root);
     }
 
-    private void applyStyleSheet(Stylesheet stylesheet) {
-        applyStylerule((Stylerule) stylesheet.getChildren().get(0));
-    }
-
-    private void applyStylerule(Stylerule rule) {
-        for (ASTNode node : rule.getChildren()) {
-            if (node instanceof Declaration) {
-                applyDeclaration((Declaration) node);
+    private void evaluateStylesheet(Stylesheet stylesheet) {
+        variableValues.addFirst(new HashMap<>());
+        for (ASTNode node : stylesheet.getChildren()) {
+            if (node instanceof VariableAssignment) {
+                evaluateVariableAssignment((VariableAssignment) node);
+                stylesheet.removeChild(node);
+            } else if (node instanceof Stylerule) {
+                evaluateStylerule((Stylerule) node);
             }
+        }
+
+        variableValues.removeFirst();
+    }
+
+    private void evaluateVariableAssignment(VariableAssignment variableAssignment) {
+        Literal value = null;
+
+        if (variableAssignment.expression instanceof Literal) {
+            value = (Literal) variableAssignment.expression;
+        } else if (variableAssignment.expression instanceof VariableReference) {
+            value = evaluateVariableLiteral(((VariableReference) variableAssignment.expression).name);
+        } else if (variableAssignment.expression instanceof Operation) {
+            value = evaluateOperation((Operation) variableAssignment.expression);
+        }
+
+        variableValues.getFirst().put(variableAssignment.name.name, value);
+    }
+
+    private Literal evaluateVariableLiteral(String variableName) {
+        for (int i = 0; i < variableValues.getSize(); i++) {
+            var value = variableValues.get(i).get(variableName);
+            if (value != null) {
+                return value;
+            }
+        }
+        return null;
+    }
+
+    private Literal evaluateOperation(Operation operation) {
+        Literal leftHandSide;
+        Literal rightHandSide;
+
+        if (operation.lhs instanceof Operation) {
+            leftHandSide = evaluateOperation((Operation) operation.lhs);
+        } else if (operation.lhs instanceof VariableReference) {
+            leftHandSide = evaluateVariableLiteral(((VariableReference) operation.lhs).name);
+        } else {
+            leftHandSide = (Literal) operation.lhs;
+        }
+
+        if (operation.rhs instanceof Operation) {
+            rightHandSide = evaluateOperation((Operation) operation.rhs);
+        } else if (operation.rhs instanceof VariableReference) {
+            rightHandSide = evaluateVariableLiteral(((VariableReference) operation.rhs).name);
+        } else {
+            rightHandSide = (Literal) operation.rhs;
+        }
+
+        if (operation instanceof MultiplyOperation) {
+            return evaluateMultiplyOperation(leftHandSide, rightHandSide);
+        } else if (operation instanceof AddOperation) {
+            return evaluateAddOperation(leftHandSide, rightHandSide);
+        } else if (operation instanceof SubtractOperation) {
+            return evaluateSubtractOperation(leftHandSide, rightHandSide);
+        }
+
+        return null;
+    }
+
+    private Literal evaluateMultiplyOperation(Literal leftHandSide, Literal rightHandSide) {
+        int leftHandValue = getLiteralAsInt(leftHandSide);
+        int rightHandValue = getLiteralAsInt(rightHandSide);
+        int result = leftHandValue * rightHandValue;
+
+        if (leftHandSide instanceof PercentageLiteral || rightHandSide instanceof PercentageLiteral) {
+            return new PercentageLiteral(result);
+        } else if (leftHandSide instanceof PixelLiteral || rightHandSide instanceof PixelLiteral) {
+            return new PixelLiteral(result);
+        } else {
+            return new ScalarLiteral(result);
         }
     }
 
-    private void applyDeclaration(Declaration declaration) {
-        declaration.expression = evaluateExpression(declaration.expression);
+    private Literal evaluateAddOperation(Literal leftHandSide, Literal rightHandSide) {
+        int leftHandValue = getLiteralAsInt(leftHandSide);
+        int rightHandValue = getLiteralAsInt(rightHandSide);
+        int result = leftHandValue + rightHandValue;
+
+        if (leftHandSide instanceof PercentageLiteral || rightHandSide instanceof PercentageLiteral) {
+            return new PercentageLiteral(result);
+        } else if (leftHandSide instanceof PixelLiteral || rightHandSide instanceof PixelLiteral) {
+            return new PixelLiteral(result);
+        } else {
+            return new ScalarLiteral(result);
+        }
     }
 
-    private Expression evaluateExpression(Expression expression) {
-       if (expression instanceof Literal) {
-           return (Literal) expression;
-       } else {
-           return evaluateAddOperation((AddOperation) expression);
-       }
+    private Literal evaluateSubtractOperation(Literal leftHandSide, Literal rightHandSide) {
+        int leftHandValue = getLiteralAsInt(leftHandSide);
+        int rightHandValue = getLiteralAsInt(rightHandSide);
+        int result = leftHandValue - rightHandValue;
+
+        if (leftHandSide instanceof PercentageLiteral || rightHandSide instanceof PercentageLiteral) {
+            return new PercentageLiteral(result);
+        } else if (leftHandSide instanceof PixelLiteral || rightHandSide instanceof PixelLiteral) {
+            return new PixelLiteral(result);
+        } else {
+            return new ScalarLiteral(result);
+        }
     }
 
-    private Expression evaluateAddOperation(AddOperation expression) {
-        PixelLiteral left = (PixelLiteral) evaluateExpression(expression.lhs);
-        PixelLiteral right = (PixelLiteral) evaluateExpression(expression.rhs);
-        return new PixelLiteral(left.value + right.value);
+    private int getLiteralAsInt(Literal leftHandSide) {
+        if (leftHandSide instanceof PercentageLiteral) {
+            return ((PercentageLiteral) leftHandSide).value;
+        } else if (leftHandSide instanceof PixelLiteral) {
+            return ((PixelLiteral) leftHandSide).value;
+        } else if (leftHandSide instanceof ScalarLiteral) {
+            return ((ScalarLiteral) leftHandSide).value;
+        }
+        return 0;
+    }
+
+    private void evaluateStylerule(Stylerule node) {
+        variableValues.addFirst(new HashMap<>());
+        for (ASTNode child : node.getChildren()) {
+            if (child instanceof VariableAssignment) {
+                evaluateVariableAssignment((VariableAssignment) child);
+                node.removeChild(child);
+            } else if (child instanceof Declaration) {
+                evaluateDeclaration((Declaration) child);
+            } else if (child instanceof IfClause) {
+                evaluateIfClause((IfClause) child);
+            }
+        }
+        variableValues.removeFirst();
     }
 }
